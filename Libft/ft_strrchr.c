a2bf51629f731809c363b38fcbb7d9e9c0415205
rvela-fe <rvela.fe@gmail.com>
2022-10-04 21:51:32 +0200

    ft_strrchr

--- Libft/ft_strrchr.c
+++ Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rvela-fe <rvela-fe@student.barcel>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 21:42:31 by rvela-fe          #+#    #+#             */
-/*   Updated: 2022/10/03 22:02:25 by rvela-fe         ###   ########.fr       */
+/*   Updated: 2022/10/04 21:48:28 by rvela-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /* Devuelve un puntero en la última ocurrencia del carácter c en la string s. */
@@ -15,7 +15,7 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	i;
+	size_t			i;
 
 	i = ft_strlen(s);
 	if ((unsigned char)c == '\0')
@@ -27,7 +27,7 @@ char	*ft_strrchr(const char *s, int c)
 		i--;
 		if (*(s + i) == (unsigned char) c)
 		{
-			return ((char *)(s + i));
+			return ((char *)s + i);
 		}
 	}
 	return ((char *) '\0');
