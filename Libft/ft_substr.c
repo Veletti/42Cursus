9cf2516aa44256c4bf8ce459c47e7e436ecac5a4
rvela-fe <rvela.fe@gmail.com>
2022-12-01 20:40:27 +0100

    substr_update

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rvela-fe <rvela-fe@student.barcel>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:34:12 by rvela-fe          #+#    #+#             */
-/*   Updated: 2022/11/29 21:19:51 by rvela-fe         ###   ########.fr       */
+/*   Updated: 2022/12/01 20:39:59 by rvela-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_strdup(""));
 	if (start + len > i)
 		len = i - start;
-	str = (char *)malloc(sizeof(*s) * (len + 1));
+	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	ft_memcpy(str, s + start, len);
