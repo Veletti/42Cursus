1a889c2b60cdc6c5f7c01cba55e6ebd1eab258f1
rvela-fe <rvela.fe@gmail.com>
2022-12-01 21:06:22 +0100

    striteri_update

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: rvela-fe <rvela-fe@student.42barc...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 20:53:27 by rvela-fe          #+#    #+#             */
-/*   Updated: 2022/10/22 17:14:24 by rvela-fe         ###   ########.fr       */
+/*   Updated: 2022/12/01 21:01:59 by rvela-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	unsigned int	i;
+	int	i;
 
 	i = 0;
 	if (s != NULL && f != NULL)
