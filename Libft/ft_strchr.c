4381a315b72683afeda7172ba7f279f9c668e390
Ruben Vela Ferres <rvela-fe@student.42barcelona.com>
2022-12-03 18:22:21 +0100

    strchr_update

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rvela-fe <rvela-fe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 17:28:03 by rvela-fe          #+#    #+#             */
-/*   Updated: 2022/12/03 18:15:01 by rvela-fe         ###   ########.fr       */
+/*   Updated: 2022/12/03 18:21:12 by rvela-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	unsigned char	len;
+	unsigned char	ch;
 
-	len = c;
+	ch = c;
 	while (*str != '\0')
 	{
-		if (*str == len)
+		if (*str == ch)
 		{
 			return ((char *) str);
 		}
 		str++;
 	}
-	if (len == '\0')
+	if (ch == '\0')
 	{
 		return ((char *) str);
 	}
