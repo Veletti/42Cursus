65dcf3638b26a642398b4f925e9eb63391f1d0b9
rvela-fe <rvela.fe@gmail.com>
2022-12-04 18:12:03 +0100

    bzero.upgrade

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rvela-fe <rvela-fe@student.42barc...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 19:16:00 by rvela-fe          #+#    #+#             */
-/*   Updated: 2022/10/13 13:31:06 by rvela-fe         ###   ########.fr       */
+/*   Updated: 2022/12/04 17:26:41 by rvela-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,3 @@ void	ft_bzero(void *s, size_t n)
 		n--;
 	}
 }
-
-/*La función bzero() pone a cero los n primeros bytes de la
-  cadena de bytes s. Si n es cero, bzero() no hace nada. */
