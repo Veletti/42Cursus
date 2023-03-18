4a6c477be1b18d9e4009a84b57c3b46c3206c7a0
Ruben Vela Ferres <rvela-fe@student.42barcelona.com>
2023-03-18 19:26:31 +0100

    ModifyMake

--- Printf/ft_printf.h
+++ Printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rvela-fe <rvela-fe@student.barcel>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 01:08:31 by rvela-fe          #+#    #+#             */
-/*   Updated: 2023/03/18 01:41:15 by rvela-fe         ###   ########.fr       */
+/*   Updated: 2023/03/18 19:10:26 by rvela-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,14 @@ void	arg_list(va_list va, char type);
 void	ft_hexadecimal(unsigned long long int num, char type);
 void	ft_pointer(unsigned long long int ptr);
 void	ft_putunsignednb(unsigned int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
 int		ft_printf(char const *str, ...);
 int		arg_count(va_list va, char type, int len);
 int		ft_len_hexadecimal(unsigned long long int num);
 int		ft_len_char(char *s);
 int		ft_len_number(int n);
+
 
 #endif
